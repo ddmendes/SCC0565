120f4ffe5fb962bd7d3b2b053ebbfb855c8ee7e8
Davi Diorio Mendes <ddioriomendes@gmail.com>
2014-10-16 13:17:34 -0300

    Não consegui fazer em python.

--- service/CadastroService.py
+++
@@ -1,8 +0,0 @@
-from ../model/UserModel import *
-from ../repository/UserRepository import *
-
-def SaveUser(user):
-	assert isinstace(user, UserModel);
-	userRepository = new UserRepository();
-	userRepository.put(user);
-	userRepository.save();
